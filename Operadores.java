2d3eb854326d77edd9f3b453390086536a1880a0
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2023-10-17 10:49:11 -0300

    Módulo 3.2 - Operadores

---
+++ Operadores.java
@@ -0,0 +1,73 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+        //Operadores de atribuição
+        int idade = 25;
+        System.out.println(System.lineSeparator() + "Valor atribuído à idade: " + idade + System.lineSeparator());
+
+
+        //Operadores matemáticos
+        int soma = idade + 5;
+        System.out.println("Resultado da soma: " + soma + System.lineSeparator());
+        
+        int multiplicacao = soma * 2;
+        System.out.println("Resultado da multiplicação: " + multiplicacao + System.lineSeparator());
+        
+        int divisao = multiplicacao / 2;
+        System.out.println("Resultado da divisão: " + divisao + System.lineSeparator());
+
+        int modulo = divisao % 4;
+        System.out.println("Resultado do módulo: " + modulo + System.lineSeparator());
+
+        
+        //Incremento e decremento
+        int incremento = 15;
+        System.out.println("Valor antes do incremento: " + incremento);
+        incremento++;
+        System.out.println("Valor depois do incremento: " + incremento + System.lineSeparator());
+
+        int decremento = 20;
+        System.out.println("Valor antes do decremento: " + decremento);
+        decremento--;
+        System.out.println("Valor depois do decremento: " + decremento + System.lineSeparator());
+
+
+        //Operadores de igualdade
+        long valor1 = 1L;
+        long valor2 = 2L;
+
+        if(valor1 == valor2) {
+            System.out.println("Valores iguais");
+        } else {
+             System.out.println("Valores diferentes");
+        }
+
+
+        double valor3 = 10.2;
+        double valor4 = 100.2;
+
+        if(valor3 != valor4) {
+            System.out.println("Valores diferentes" + System.lineSeparator());
+        } else {
+             System.out.println("Valores iguais");
+        }
+        
+
+        //Operadores relacionais
+        long valor5 = 1234;
+        long valor6 = 12345;
+
+        if(valor5 > valor6) {System.out.println(valor5 + " é maior que " + valor6);}
+        if(valor5 >= valor6) {System.out.println(valor5 + " é maior ou igual que " + valor6);}
+        if(valor5 < valor6) {System.out.println(valor5 + " é menor que " + valor6);}
+        if(valor5 <= valor6) {System.out.println(valor5 + " é menor ou igual que " + valor6 + System.lineSeparator());}
+
+
+        //Operadores lógicos
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        System.out.println(verdadeiro || falso);
+        System.out.println(verdadeiro && falso);
+    }
+}
--- VariaveisETiposDeDados.java
+++ VariaveisETiposDeDados.java
@@ -4,44 +4,35 @@ public class VariaveisETiposDeDados {
 
         //8 bits - Min.: -128 / Max.: 127
         byte meuByte = 127;
+        System.out.println(System.lineSeparator() + "Meu Byte: " + meuByte + System.lineSeparator());
 
         //16 bits - Min.: -32768 / Max.: 32767
         short meuShort = 32767;
+        System.out.println("Meu Short: " + meuShort + System.lineSeparator());
 
         //32 bits - Min.: -2147483648 / Max.: 2147483647
         int meuInt = 2147483647;
+        System.out.println("Meu Int: " + meuInt + System.lineSeparator());
 
         //64 bits
         long meuLong = 10L;
+        System.out.println("Meu Long: " + meuLong + System.lineSeparator());
 
         //64 bits - Dupla precisão
         double meuDouble = 102.51;
+        System.out.println("Meu Double: " + meuDouble + System.lineSeparator());
 
         //32 bits - Precisão simples
         float meuFloat = 102.51F;
+        System.out.println("Meu Float: " + meuFloat + System.lineSeparator());
 
         //16 bits - 1 unicode
         char meuChar = 'A';
+        System.out.println("Meu Char: " + meuChar + System.lineSeparator());
 
         //1 bit
         boolean meuBoolean = true;
-
-
-        System.out.println("Meu Byte: " + meuByte);
-
-        System.out.println("Meu Short: " + meuShort);
-
-        System.out.println("Meu Int: " + meuInt);
-
-        System.out.println("Meu Long: " + meuLong);
-
-        System.out.println("Meu Double: " + meuDouble);
-
-        System.out.println("Meu Float: " + meuFloat);
-
-        System.out.println("Meu Char: " + meuChar);
-
-        System.out.println("Meu Boolean: " + meuBoolean);
+        System.out.println("Meu Boolean: " + meuBoolean + System.lineSeparator());
 
     }
 }
